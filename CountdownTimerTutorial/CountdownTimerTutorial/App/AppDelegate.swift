4dcae6f30372eed93a5207583bf669b40ab0c3f6
Marlon <marlonr@hey.com>
2024-12-16 01:55:15 -0500

    Starter branch setup complete

---
+++ CountdownTimerTutorial/CountdownTimerTutorial/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CountdownTimerTutorial
+//
+//  Created by Marlon Raskin on 2024-12-16.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+	) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
---
+++ CountdownTimerTutorial/CountdownTimerTutorial/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CountdownTimerTutorial
+//
+//  Created by Marlon Raskin on 2024-12-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .backgroundPrimary
+	}
+}
+
